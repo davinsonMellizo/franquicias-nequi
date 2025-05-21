d9331e8e74b80a03d5344e001ecb90fbdc6cf3a2
dmellizo <dmellizo@bancolombia.com.co>
2025-05-21 10:03:10 -0500

    feat: model creation

---
+++ domain/model/src/main/java/co/com/nequi/model/branch/gateways/BranchRepository.java
@@ -0,0 +1,4 @@
+package co.com.nequi.model.branch.gateways;
+
+public interface BranchRepository {
+}
---
+++ domain/model/src/main/java/co/com/nequi/model/franchise/gateways/FranchiseRepository.java
@@ -0,0 +1,4 @@
+package co.com.nequi.model.franchise.gateways;
+
+public interface FranchiseRepository {
+}
